5a6951d29e5423be1fde58726af05e5e44e0e916
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-21 23:39:55 +0900

    feat: 제품 초기화, 업데이트, 추가

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -2,9 +2,26 @@ import { useState } from "react";
 import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
+  const [products, setProducts] = useState(initialProducts);
+
+  const updateProduct = (editedProduct: Product) => {
+    const updatedProducts = products.map((product) => {
+      if (editedProduct.id === product.id) {
+        return editedProduct;
+      }
+      return product;
+    });
+
+    setProducts(updatedProducts);
+  };
+
+  const addProduct = (newProduct: Product) => {
+    setProducts((prev) => [...prev, newProduct]);
+  };
+
   return {
-    products: [],
-    updateProduct: () => undefined,
-    addProduct: () => undefined,
+    products: products,
+    updateProduct,
+    addProduct,
   };
 };
