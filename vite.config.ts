fbfab57d632b7e93ab871864bc08d207af6cfd63
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2025-04-24 00:29:19 +0900

    deploy: 배포설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    base: "/front_5th_chapter2-2/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.origin.html"),
+          refactoring: resolve(__dirname, "index.refactoring.html"),
+        },
+      },
+      outDir: "dist",
+    },
   }),
   defineTestConfig({
     test: {
       globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.ts",
     },
   })
-)
+);
